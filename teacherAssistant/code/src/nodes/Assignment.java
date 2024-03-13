8fe537921d6e5fa5acc44abfdfc4054f69529259
WilliamStarling <williamjonas@comcast.net>
2024-03-13 16:09:11 -0500

    Update Assignment.java

--- teacherAssistant/code/src/nodes/Assignment.java
+++ teacherAssistant/code/src/nodes/Assignment.java
@@ -1,25 +1,34 @@
 package nodes;
+
 public class Assignment {
 
-	//Assignment attributes
+	// Assignment attributes
 	private String f_name = "new assignment";
 	private String f_description = "This is an assignment.";
-	private int f_type = 0; //0 is homework, * 1 is quiz, 2 is test/exam, and 3 is final.
+	private int f_type = 0; // 0 is homework, * 1 is quiz, 2 is test/exam, and 3 is final.
 	private int f_possiblePoints = 100;
 	private float f_grade = 100;
 	private String f_dueDate = "01/01/1970";
 
 	/*
-	 * This method is the constructor for the assignment class for the student assignment list. 
-	 * It takes the parameters given and updates the data members to hold these values.
+	 * This method is the constructor for the assignment class for the student
+	 * assignment list. It takes the parameters given and updates the data members
+	 * to hold these values.
+	 * 
 	 * @param newName: The name of the assignment
+	 * 
 	 * @param newDescription: The description of the assignment
-	 * @param newType: What type of assignment it is (homework, quiz, test, etc. user will select
-	 * 	a type and then this will be translated into an integer by upper layer.
+	 * 
+	 * @param newType: What type of assignment it is (homework, quiz, test, etc.
+	 * user will select a type and then this will be translated into an integer by
+	 * upper layer.
+	 * 
 	 * @param possiblePoints: How many points the assignment is out of.
+	 * 
 	 * @param grade: How many points were given.
 	 */
-	public void Assignment(String newName, String newDescription, int newType, int possiblePoints, String dueDate, float grade) {
+	public Assignment(String newName, String newDescription, int newType, 
+			int possiblePoints, String dueDate, float grade) {
 		f_name = newName;
 		f_description = newDescription;
 		f_type = newType;
@@ -27,17 +36,24 @@ public class Assignment {
 		f_dueDate = dueDate;
 		f_grade = grade;
 	}
+	
 	/*
-	 * This is the constructor for the Assignment class for the a class's assignment list.
-	 * It takes the parameters given and updates the data members to hold these values.
+	 * This is the constructor for the Assignment class for the a class's assignment
+	 * list. It takes the parameters given and updates the data members to hold
+	 * these values.
+	 * 
 	 * @param newName: The name of the assignment
+	 * 
 	 * @param newDescription: The description of the assignment
-	 * @param newType: What type of assignment it is (homework, quiz, test, etc. user will select
-	 * 	a type and then this will be translated into an integer by upper layer.
+	 * 
+	 * @param newType: What type of assignment it is (homework, quiz, test, etc.
+	 * user will select a type and then this will be translated into an integer by
+	 * upper layer.
+	 * 
 	 * @param possiblePoints: How many points the assignment is out of.
 	 */
-	
-	public void Assignment(String newName, String newDescription, int newType, int possiblePoints, String dueDate) {
+	public Assignment(String newName, String newDescription, 
+			int newType, int possiblePoints, String dueDate) {
 		f_name = newName;
 		f_description = newDescription;
 		f_type = newType;
@@ -45,18 +61,20 @@ public class Assignment {
 		f_dueDate = dueDate;
 	}
 
-	
 	/*
 	 * This is the setter method for the f_name data member in the class.
+	 * 
 	 * @param newName: the new name that you want to update f_name to have.
 	 */
 	public void setName(String newName) {
 		f_name = newName;
 	}
-	
+
 	/*
 	 * This is the setter method for the f_description data member in the class.
-	 * @param newDescription: the new description that you want to update f_description to have.
+	 * 
+	 * @param newDescription: the new description that you want to update
+	 * f_description to have.
 	 */
 	public void setDescription(String newDescription) {
 		f_description = newDescription;
@@ -64,6 +82,7 @@ public class Assignment {
 
 	/*
 	 * This is the setter method for the f_type data member in the class.
+	 * 
 	 * @param newType: the new type that you want to update f_type to have.
 	 */
 	public void setType(int newType) {
@@ -72,7 +91,9 @@ public class Assignment {
 
 	/*
 	 * This is the setter method for the f_possiblePoints data member in the class.
-	 * @param newPossible: the new max that you want to update f_possiblePoints to have.
+	 * 
+	 * @param newPossible: the new max that you want to update f_possiblePoints to
+	 * have.
 	 */
 	public void setPossiblePoints(int newPossible) {
 		f_possiblePoints = newPossible;
@@ -80,8 +101,9 @@ public class Assignment {
 
 	/*
 	 * This is the setter method for the f_grade data member in the class.
-	 * @param newGrade: the new grade that you want to update f_grade to have.
-	 * 0 is homework, 1 is quiz, 2 is test/exam, and 3 is final.
+	 * 
+	 * @param newGrade: the new grade that you want to update f_grade to have. 0 is
+	 * homework, 1 is quiz, 2 is test/exam, and 3 is final.
 	 */
 	public void setGrade(int newGrade) {
 		f_grade = newGrade;
@@ -89,15 +111,17 @@ public class Assignment {
 
 	/*
 	 * This is the setter method for the f_dueDate data member in the class.
-	 * @param newDueDate: the new due date that you want to update f_dueDate to have.
+	 * 
+	 * @param newDueDate: the new due date that you want to update f_dueDate to
+	 * have.
 	 */
 	public void setDueDate(String newDueDate) {
 		f_dueDate = newDueDate;
 	}
-	
-	
+
 	/*
 	 * This is the getter method for the f_name data member in the class.
+	 * 
 	 * @return: returns the name of the assignment.
 	 */
 	public String getName() {
@@ -106,6 +130,7 @@ public class Assignment {
 
 	/*
 	 * This is the getter method for the f_descrption data member in the class.
+	 * 
 	 * @return: returns the description of the assignment.
 	 */
 	public String getDescription() {
@@ -114,6 +139,7 @@ public class Assignment {
 
 	/*
 	 * This is the getter method for the f_possiblePoints data member in the class.
+	 * 
 	 * @return: returns the max points someone can earn on the assignment.
 	 */
 	public int getPossiblePoints() {
@@ -122,8 +148,9 @@ public class Assignment {
 
 	/*
 	 * This is the getter method for the f_type data member in the class.
-	 * @return: returns an integer that corresponds to a specific type. 0 is homework,
-	 * 1 is quiz, 2 is test/exam, and 3 is final.
+	 * 
+	 * @return: returns an integer that corresponds to a specific type. 0 is
+	 * homework, 1 is quiz, 2 is test/exam, and 3 is final.
 	 */
 	public int getType() {
 		return f_type;
@@ -131,14 +158,16 @@ public class Assignment {
 
 	/*
 	 * This is the getter method for the f_grade data member in the class.
+	 * 
 	 * @return: returns the grade someone has on the assignment.
 	 */
 	public float getGrade() {
 		return f_grade;
 	}
-	
+
 	/*
 	 * This is the getter method for the f_dueDate data member in the class.
+	 * 
 	 * @return: returns the due date of the assignment.
 	 */
 	public String getDueDate() {
