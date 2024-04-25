c8350a05e97ea134eb867954a5aed2337e1d4272
savannahHebert <smh2222@jagmail.southalabama.edu>
2024-04-25 10:18:55 -0500

    console works
    console works now

--- teacherAssistant/code/src/ui/Console.java
+++ teacherAssistant/code/src/ui/Console.java
@@ -14,11 +14,10 @@ public class Console {
 	 * 
 	 * @param args:
 	 */
-	 /*
+	 
 	public static void main(String[] args) {
 		MainMenu();
 	}
-	*/
 	
 	/*
 	 * This is the main menu that's called, which handles calling different functions to show options,
--- teacherAssistant/code/src/ui/Launch.java
+++ teacherAssistant/code/src/ui/Launch.java
@@ -5,11 +5,11 @@ import application.Control;
 public class Launch {
 
 	static Control newSession = new Control(); //For completing actions
-	
+	/*
 	public static void main(String[] args) {
 		
 		
 		new Log_In_Frame(newSession);
 	}
-
+	*/
 }
